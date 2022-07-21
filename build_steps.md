37b1972d38b06994191a18e01c828749d20fb4ac
zeyuLin <2352456947@qq.com>
2022-07-21 14:28:08 +0800

    add antdvue&pinia&unplugin-vue-components  && set alias&scss in vite.config.ts

---
+++ build_steps.md
@@ -0,0 +1,12 @@
+## How to init this Vue project?
+
+pnpm add sass -D
+
+pnpm add eslint eslint-plugin-vue -D
+pnpm add prettier eslint-plugin-prettier @vue/eslint-config-prettier -D
+pnpm add @vue/eslint-config-typescript @typescript-eslint/eslint-plugin @typescript-eslint/parser -D
+
+pnpm add ant-design-vue
+pnpm add unplugin-vue-components -D
+
+pnpm add pinia
--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from '@/components/HelloWorld.vue'
 </script>
 
 <template>
@@ -14,6 +15,7 @@ import HelloWorld from './components/HelloWorld.vue'
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
+  <a-rate :value="5" />
 </template>
 
 <style scoped>
